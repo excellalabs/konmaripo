d477b3a8b84ff6e2a447b9e7bef84d5bf115c2f5
Sean Killeen <SeanKilleen@gmail.com>
2020-06-05 15:35:36 -0400

    Packages and a test of tests

---
+++ src/Konmaripo.Web.Tests.Unit/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace Konmaripo.Web.Tests.Unit
+{
+    public class MyDoodadTests
+    {
+        [Fact]
+        public void OnePlusOneShouldBeTwo()
+        {
+            var result = 1 + 1;
+
+            Assert.Equal(2, result);
+        }
+    }
+}
