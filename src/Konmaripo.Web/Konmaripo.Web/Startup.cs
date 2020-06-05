d50775a66f719ccbf0757f57dca14d2f5c23f928
Sean Killeen <SeanKilleen@gmail.com>
2020-06-05 09:47:38 -0400

    Add configuration for forwarding headers

--- src/Konmaripo.Web/Konmaripo.Web/Startup.cs
+++ src/Konmaripo.Web/Konmaripo.Web/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
@@ -26,11 +27,17 @@ namespace Konmaripo.Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureHackyHttpsEnforcement(services);
+
             IdentityModelEventSource.ShowPII = true;
             services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
-                .AddAzureAD(options => Configuration.Bind("AzureAd", options));
+                .AddAzureAD(options =>
+                {
+                    Configuration.Bind("AzureAd", options);
+                });
 
             services.AddControllersWithViews(options =>
             {
@@ -40,6 +47,27 @@ namespace Konmaripo.Web
                 options.Filters.Add(new AuthorizeFilter(policy));
             });
             services.AddRazorPages();
+        }
+
+        /// <summary>
+        /// This is a workaround due to issues running in a Linux container with no reverse proxy in front of it.
+        /// In this situation, Azure AD authentication attempts to use a redirect URI that is http instead of https.
+        /// For more information: https://github.com/excellalabs/konmaripo/issues/10
+        /// </summary>
+        /// <param name="services"></param>
+        private void ConfigureHackyHttpsEnforcement(IServiceCollection services)
+        {
+            // HACK
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+                                           ForwardedHeaders.XForwardedProto;
+                // Only loopback proxies are allowed by default.
+                // Clear that restriction because forwarders are enabled by explicit 
+                // configuration.
+                options.KnownNetworks.Clear();
+                options.KnownProxies.Clear();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
