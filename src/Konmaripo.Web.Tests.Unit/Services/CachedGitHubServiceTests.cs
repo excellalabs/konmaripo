19b2439f515ec248207ba01d3ba7f4657a9082cc
Sean Killeen <SeanKilleen@gmail.com>
2020-06-10 14:58:18 -0400

    Basic test class & Ctor test

---
+++ src/Konmaripo.Web.Tests.Unit/Services/CachedGitHubServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Konmaripo.Web.Services;
+using Xunit;
+
+namespace Konmaripo.Web.Tests.Unit.Services
+{
+    public class CachedGitHubServiceTests
+    {
+        public class Ctor
+        {
+            [Fact]
+            public void WithNullGithubService_ThrowsException()
+            {
+                Action act = () => new CachedGitHubService(null);
+
+                act.Should().Throw<ArgumentNullException>()
+                    .And.ParamName.Should().Be("gitHubService");
+            }
+        }
+    }
+}
---
+++ src/Konmaripo.Web/Services/CachedGitHubService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Konmaripo.Web.Services
+{
+    public class CachedGitHubService
+    {
+        private IGitHubService _gitHubService;
+        public CachedGitHubService(IGitHubService gitHubService)
+        {
+            _gitHubService = gitHubService ?? throw new ArgumentNullException(nameof(gitHubService));
+        }
+    }
+}
