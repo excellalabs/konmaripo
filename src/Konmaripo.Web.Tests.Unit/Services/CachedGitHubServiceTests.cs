90abda9dec825b51426b99ec57327435f602d504
Sean Killeen <SeanKilleen@gmail.com>
2020-06-10 15:05:12 -0400

    Some tests

--- src/Konmaripo.Web.Tests.Unit/Services/CachedGitHubServiceTests.cs
+++ src/Konmaripo.Web.Tests.Unit/Services/CachedGitHubServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Konmaripo.Web.Services;
+using Moq;
 using Xunit;
 
 namespace Konmaripo.Web.Tests.Unit.Services
@@ -18,6 +20,36 @@ namespace Konmaripo.Web.Tests.Unit.Services
 
                 act.Should().Throw<ArgumentNullException>()
                     .And.ParamName.Should().Be("gitHubService");
+            }
+        }
+        public class GetRepositoriesForOrganizationAsync
+        {
+            private Mock<IGitHubService> _mockService;
+            private CachedGitHubService _sut;
+            public GetRepositoriesForOrganizationAsync()
+            {
+                _sut = new CachedGitHubService(_mockService.Object);
+            }
+
+            [Fact]
+            public async Task WhenCalled_CallsUnderlyingGitHubService()
+            {
+                await _sut.GetRepositoriesForOrganizationAsync();
+
+                _mockService.Verify(x=>x.GetRepositoriesForOrganizationAsync(), Times.Once);
+            }
+
+            [Fact]
+            public void WhenCalledMultipleTimes_StillGetsFullRepositoryListFromUnderlyingService()
+            {
+                throw new NotImplementedException();
+            }
+
+
+            [Fact]
+            public void WhenCalledMultipleTimes_CallsUnderlyingGitHubServiceOnlyOnce()
+            {
+                throw new NotImplementedException();
             }
         }
     }
--- src/Konmaripo.Web/Services/CachedGitHubService.cs
+++ src/Konmaripo.Web/Services/CachedGitHubService.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Konmaripo.Web.Models;
 
 namespace Konmaripo.Web.Services
 {
@@ -11,6 +13,12 @@ namespace Konmaripo.Web.Services
         public CachedGitHubService(IGitHubService gitHubService)
         {
             _gitHubService = gitHubService ?? throw new ArgumentNullException(nameof(gitHubService));
+        }
+
+        public async Task<List<GitHubRepo>> GetRepositoriesForOrganizationAsync()
+        {
+            var result = await _gitHubService.GetRepositoriesForOrganizationAsync();
+            return result;
         }
     }
 }
