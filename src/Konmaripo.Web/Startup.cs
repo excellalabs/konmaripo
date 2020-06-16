7fc532ff289e4e8883ef20d47164a7dfc9575990
Sean Killeen <SeanKilleen@gmail.com>
2020-06-16 14:43:44 -0400

    Revert "Revert "inject serilog logger""
    This reverts commit 1bda3d04e6499cc2cc244039bb7c5bad1d8434bf.

--- src/Konmaripo.Web/Startup.cs
+++ src/Konmaripo.Web/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Octokit;
 using Octokit.Internal;
@@ -65,7 +64,7 @@ namespace Konmaripo.Web
             {
                 var gitHubClient = provider.GetRequiredService<GitHubClient>();
                 var githubSettings = provider.GetService<IOptions<GitHubSettings>>();
-                var logger = provider.GetService<ILogger<GitHubService>>();
+                var logger = provider.GetService<ILogger>();
 
                 var service = new GitHubService(gitHubClient, githubSettings, logger);
 
