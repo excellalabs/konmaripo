6713d69b2c46dcaeca775f790333ade020a41b89
Sean Killeen <SeanKilleen@gmail.com>
2020-06-22 11:19:36 -0400

    Copy/adaptation of Sunsetting controller
    Being lazy. :)

---
+++ src/Konmaripo.Web.Tests.Unit/Controllers/MassIssuesControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using Konmaripo.Web.Controllers;
+using Konmaripo.Web.Services;
+using Moq;
+using Serilog;
+using Xunit;
+
+namespace Konmaripo.Web.Tests.Unit.Controllers
+{
+    public class MassIssuesControllerTests
+    {
+        [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
+        public class Ctor
+        {
+            private readonly Mock<ILogger> _mockLogger;
+            private readonly Mock<IGitHubService> _mockGitHubService;
+
+            public Ctor()
+            {
+                _mockLogger = new Mock<ILogger>();
+                _mockGitHubService = new Mock<IGitHubService>();
+            }
+
+            [Fact]
+            public void NullLogger_ThrowsException()
+            {
+                Action act = () => new MassIssuesController(null, _mockGitHubService.Object);
+
+                act.Should().Throw<ArgumentNullException>()
+                    .And.ParamName.Should().Be("logger");
+            }
+
+            [Fact]
+            public void NullGitHubService_ThrowsException()
+            {
+                Action act = () => new MassIssuesController(_mockLogger.Object, null);
+
+                act.Should().Throw<ArgumentNullException>()
+                    .And.ParamName.Should().Be("gitHubService");
+            }
+        }
+    }
+}
---
+++ src/Konmaripo.Web/Controllers/MassIssuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Konmaripo.Web.Models;
+using Konmaripo.Web.Services;
+using Serilog;
+
+namespace Konmaripo.Web.Controllers
+{
+    [Authorize]
+    public class MassIssuesController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly IGitHubService _gitHubService;
+
+        public MassIssuesController(ILogger logger, IGitHubService gitHubService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _gitHubService = gitHubService ?? throw new ArgumentNullException(nameof(gitHubService));
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Obtain list of GitHub Repos
+            // Pass through to the view
+            var repos = await _gitHubService.GetRepositoriesForOrganizationAsync();
+
+            _logger.Information("Returning {RepoCount} repositories", repos.Count);
+
+            return View(repos);
+
+        }
+    }
+}
