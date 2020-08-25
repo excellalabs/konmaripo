4234d372fceb681286a68364f0dc5757d93a1848
Sean Killeen <SeanKilleen@gmail.com>
2020-08-25 11:28:45 -0400

    Add to home page

--- src/Konmaripo.Web/Controllers/HomeController.cs
+++ src/Konmaripo.Web/Controllers/HomeController.cs
@@ -4,27 +4,44 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Konmaripo.Web.Models;
 using Konmaripo.Web.Services;
+using Microsoft.Extensions.Options;
 using Serilog;
 using Activity = System.Diagnostics.Activity;
 
 namespace Konmaripo.Web.Controllers
 {
+    public class HomeIndexViewModel
+    {
+        public RepoQuota RepoQuota { get; }
+        public string OrgName { get; }
+
+        public HomeIndexViewModel(RepoQuota repoQuota, string orgName)
+        {
+            RepoQuota = repoQuota;
+            OrgName = orgName;
+        }
+    }
+
     [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger _logger;
         private readonly IGitHubService _gitHubService;
+        private readonly GitHubSettings _gitHubSettings;
 
-        public HomeController(ILogger logger, IGitHubService gitHubService)
+        public HomeController(ILogger logger, IGitHubService gitHubService, IOptions<GitHubSettings> gitHubSettings)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _gitHubService = gitHubService ?? throw new ArgumentNullException(nameof(gitHubService));
+            _gitHubSettings = gitHubSettings?.Value ?? throw new ArgumentNullException(nameof(gitHubSettings));
         }
 
         public async Task<IActionResult> Index()
         {
             var repoQuota = await _gitHubService.GetRepoQuotaForOrg();
-            return View(repoQuota);
+            var orgName = _gitHubSettings.OrganizationName;
+            var vm = new HomeIndexViewModel(repoQuota, orgName);
+            return View(vm);
         }
 
         [AllowAnonymous]
