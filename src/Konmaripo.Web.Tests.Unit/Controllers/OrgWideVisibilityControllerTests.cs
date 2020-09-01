0da99d168e07574d8277c02762207e8cb7fc833a
Sean Killeen <SeanKilleen@gmail.com>
2020-09-01 10:23:48 -0400

    visibility settings param and tests

---
+++ src/Konmaripo.Web.Tests.Unit/Controllers/OrgWideVisibilityControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Konmaripo.Web.Controllers;
+using Xunit;
+
+namespace Konmaripo.Web.Tests.Unit.Controllers
+{
+    public class OrgWideVisibilityControllerTests
+    {
+        public class Ctor
+        {
+            [Fact]
+            public void NoVisibilitySettings_ThrowsError()
+            {
+                Action act = () => new OrgWideVisibilityController(null);
+
+                act.Should().Throw<ArgumentNullException>()
+                    .And.ParamName.Should().Be("visibilitySettings");
+            }
+        }
+    }
+}
--- src/Konmaripo.Web/Controllers/OrgWideVisibilityController.cs
+++ src/Konmaripo.Web/Controllers/OrgWideVisibilityController.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Konmaripo.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.Extensions.Options;
 
 namespace Konmaripo.Web.Controllers
 {
     public class OrgWideVisibilityController : Controller
     {
+        public OrgWideVisibilityController(IOptions<OrgWideVisibilitySettings> visibilitySettings)
+        {
+            if (visibilitySettings == null){throw new ArgumentNullException(nameof(visibilitySettings));}
+        }
         public IActionResult Index()
         {
+
             return View();
         }
     }
