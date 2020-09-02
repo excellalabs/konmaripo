9a2ba55155580cf2cc478f08430cb60d5e6e4432
Sean Killeen <SeanKilleen@gmail.com>
2020-09-02 16:25:19 -0400

    refactor: store _settings object itself

--- src/Konmaripo.Web/Controllers/OrgWideVisibilityController.cs
+++ src/Konmaripo.Web/Controllers/OrgWideVisibilityController.cs
@@ -12,23 +12,25 @@ namespace Konmaripo.Web.Controllers
 {
     public class OrgWideVisibilityController : Controller
     {
-        private string _orgWideTeam;
+        private OrgWideVisibilitySettings _settings;
         private IGitHubService _gitHubService;
 
         public OrgWideVisibilityController(IOptions<OrgWideVisibilitySettings> visibilitySettings, IGitHubService gitHubService)
         {
             if (visibilitySettings == null){throw new ArgumentNullException(nameof(visibilitySettings));}
             if (string.IsNullOrWhiteSpace(visibilitySettings.Value.AllOrgMembersGroupName)){throw new ArgumentNullException(nameof(visibilitySettings.Value.AllOrgMembersGroupName));}
-            _orgWideTeam = visibilitySettings.Value.AllOrgMembersGroupName;
+
+            _settings = visibilitySettings.Value;
             _gitHubService = gitHubService ?? throw new ArgumentNullException(nameof(gitHubService));
         }
         public async Task<IActionResult> Index()
         {
-            var teamExists = await _gitHubService.TeamExists(_orgWideTeam);
+            var teamExists = await _gitHubService.TeamExists(_settings.AllOrgMembersGroupName);
 
-            var vm = new OrgWideVisibilityIndexVM(_orgWideTeam, teamExists);
+            var vm = new OrgWideVisibilityIndexVM(_settings.AllOrgMembersGroupName, teamExists);
             return View(vm);
         }
+
     }
 
     public class OrgWideVisibilityIndexVM
