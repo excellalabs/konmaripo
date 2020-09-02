bb6ec869a95ebb6e8c546146e0ac942236e25b8d
Sean Killeen <SeanKilleen@gmail.com>
2020-09-02 16:21:20 -0400

    Add a description config

--- src/Konmaripo.Web/Models/OrgWideVisibilitySettings.cs
+++ src/Konmaripo.Web/Models/OrgWideVisibilitySettings.cs
@@ -3,5 +3,6 @@
     public class OrgWideVisibilitySettings
     {
         public string AllOrgMembersGroupName { get; set; }
+        public string AllOrgMembersGroupDescription { get; set; }
     }
 }
