dbb9ea8e770b5a0c0dc62706a79c3c38977dda23
Sean Killeen <SeanKilleen@gmail.com>
2023-02-22 10:47:23 -0500

    Add subcribers to repository view (#283)
    * Add subscribers to definition
    
    * Update code to use subscribers
    
    * Add subscribers to view

--- src/Konmaripo.Web/Services/CachedGitHubService.cs
+++ src/Konmaripo.Web/Services/CachedGitHubService.cs
@@ -58,7 +58,7 @@ namespace Konmaripo.Web.Services
             var repos = _memoryCache.Get<List<GitHubRepo>>(RepoCacheKey);
             var item = repos.First(x => x.Id == repoId);
 
-            var archivedItem = new GitHubRepo(item.Id, item.Name, item.StarCount, true, item.ForkCount, item.OpenIssueCount, item.CreatedDate, item.UpdatedDate, item.Description, item.IsPrivate, item.PushedDate.ToNullable(), item.RepoUrl);
+            var archivedItem = new GitHubRepo(item.Id, item.Name, item.StarCount, true, item.ForkCount, item.OpenIssueCount, item.CreatedDate, item.UpdatedDate, item.Description, item.IsPrivate, item.PushedDate.ToNullable(), item.RepoUrl, item.Subscribers);
             repos.Remove(item);
             repos.Add(archivedItem);
 
